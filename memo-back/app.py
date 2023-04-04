0126ce5038cccd620c125f5e66e3a772a7dd19af
Yuji Sakata <64772294+Yuji-Sakata0110@users.noreply.github.com>
2023-04-04 22:08:20 +0900

    database-setup for fastapi (#1)
    yuji done

--- memo-back/app.py
+++ memo-back/app.py
@@ -4,11 +4,16 @@ import pymongo
 import logging
 from dotenv import load_dotenv
 import os
+import json
+# avoid objectid error
+from bson.json_util import dumps
+
 
 # load .env
 load_dotenv(".env")
 # fastapi app register
 memo_back = FastAPI()
+
 
 # logger setting
 logging.basicConfig(
@@ -19,6 +24,7 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+
 
 # database connection class
 class database_connection():
@@ -34,19 +40,48 @@ class database_connection():
             logging.error(message)
             return {"message": message}
 
-    def find_one():
-        db_con = database_connection.con()
-        document = db_con.find_one()
-        return document
 
-    def find_all():
+    def fetch_one():
+        db_con = database_connection.con()
+        document = db_con.find_one({"name": "yujis"})
+        logging.info(document)
+        return json.loads(dumps(document))
+
+
+    def fetch_all():
         db_con = database_connection.con()
         # cursor type
         cursor = db_con.find()
         # cursor to list type
         documents = [document for document in cursor]
         logging.info(documents)
-        return documents
+        return json.loads(dumps(documents))
+
+
+    def insert_one():
+        db_con = database_connection.con()
+        with open('data/insert_data.json') as f:
+            data: dict = json.load(f)
+            try:
+                db_con.insert_one(data)
+                logging.debug(data)
+                logging.info("inserted to database")
+                return {"message": "insert success"}
+            except:
+                return {"message": "insert failed"}
+
+
+    def insert_many():
+        db_con = database_connection.con()
+        with open('data/insert_many_data.json') as f:
+            data: dict = json.load(f)
+            try:
+                db_con.insert_many(data)
+                logging.debug(data)
+                logging.info("inserted to database")
+                return {"message": "insert many success"}
+            except:
+                return {"message": "insert many failed"}
 
 
 # routes
@@ -54,14 +89,27 @@ class database_connection():
 async def root_get():
     return {"message": "Hello World"}
 
+
 @memo_back.post("/")
 async def root_post():
     return {"message": "Hello World, post"}
 
-@memo_back.get("/feach_one")
-async def feach_one_data():
-    return {"message": database_connection.find_one()}
 
-@memo_back.get("/feach_all")
-async def feach_all_data():
-    return {"message": database_connection.find_all()}
+@memo_back.get("/fetch_one")
+async def fetch_one_data():
+    return {"message": database_connection.fetch_one()}
+
+
+@memo_back.get("/fetch_all")
+async def fetch_all_data():
+    return {"message": database_connection.fetch_all()}
+
+
+@memo_back.get("/insert_one")
+async def insert_one_data():
+    return {"message": database_connection.insert_one()}
+
+
+@memo_back.get("/insert_many")
+async def insert_many_data():
+    return {"message": database_connection.insert_many()}
